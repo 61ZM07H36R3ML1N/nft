8e2f9062ce318b2c40ec7da92371f982b159e2d0
BT (Grem) Jamiel <brianthomasjamiel@gmail.com>
2022-09-30 00:02:18 -0700

    working on loading layers and picking randomly

--- .history/avatar_generator_20220929235339.py
+++ .history/avatar_generator_20220929235339.py
@@ -1,13 +1,9 @@
-import os
-
 class AvatarGenerator: 
     def __init__(self, images_path: str):
         self.layers = self.load_image_layers(images_path)
         pass
     
     def load_image_layers(self, images_path: str):
-        sub_paths = os.listdir(images_path).sort()
-        print(sub_paths)
         layers = []
         return layers
         
