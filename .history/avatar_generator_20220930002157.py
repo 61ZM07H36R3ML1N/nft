89127bd0e61ec674e116a011a71473884af0a68e
BT (Grem) Jamiel <brianthomasjamiel@gmail.com>
2022-09-30 00:33:52 -0700

    more load image layer chunk

--- .history/avatar_generator_20220930002157.py
+++ .history/avatar_generator_20220930002157.py
@@ -1,18 +1,16 @@
 import os
 
-from layer import Layer 
 class AvatarGenerator: 
     def __init__(self, images_path: str):
-        self.layers: List[Layer] = self.load_image_layers(images_path)
+        self.layers = self.load_image_layers(images_path)
         pass
     
     def load_image_layers(self, images_path: str):
         sub_paths = sorted(os.listdir(images_path))
+        print(sub_paths)
         layers = []
         for sub_path in sub_paths:
             layer_path = os.path.join(images_path, sub_path)
-            layer = Layer(layer_path)
-            layers.append(layer)
             print(layer_path)
         return layers
         
